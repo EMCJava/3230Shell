d84555b57eec218cb241c3039e158f127dc3edcd
lys <loyusum246807952@gmail.com>
2022-10-18 14:54:09 +0800

    Added logger for background process term

--- main.c
+++ main.c
@@ -31,7 +31,9 @@ struct Shell initShell() {
     shell.current_command = NULL;
     shell.command_stats_required = 0;
     shell.running = 1;
+    shell.background_last_pipe_process_stats = NewArray();
     shell.current_child_process_stats = NewArray();
+    shell.logger = NewArray();
 
     signal(SIGCLD, shell_child_signal_handler);
 
@@ -42,6 +44,24 @@ int main(int argc, char *argv[]) {
     g_shell = initShell();
     struct Shell *shell_ptr = &g_shell;
     while (g_shell.running) {
+
+        // log
+
+        sigset_t child_mask;
+        if ((sigemptyset(&child_mask) == -1) || (sigaddset(&child_mask, SIGCLD) == -1)) {
+            return -1;
+        }
+        if (sigprocmask(SIG_BLOCK, &child_mask, NULL) != -1) {
+
+            for (int i = 0; i < Len(g_shell.logger); ++i) {
+                printf("%s", (char *) *At(g_shell.logger, i));
+            }
+
+            FreeArrayElements(&g_shell.logger);
+            FreeArray(&g_shell.logger);
+
+            while (sigprocmask(SIG_UNBLOCK, &child_mask, NULL) == -1);
+        }
 
         // setting a jump point to exit blocking readline function
         signal(SIGINT, shell_input_signal_handler);
